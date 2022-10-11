c00d7989aaba3a3b2e647c42bd39edadf28e732b
Dongmin Kim <kimdongmin68@naver.com>
2022-10-11 22:21:43 +0900

    Refactoring

---
+++ src/main/java/com/dutmdcjf/moviereviewproject/dto/ReviewDTO.java
@@ -0,0 +1,11 @@
+package com.dutmdcjf.moviereviewproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+public class ReviewDTO {
+
+}
